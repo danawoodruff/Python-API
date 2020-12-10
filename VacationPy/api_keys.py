34e4b81727343292d056a37cfcafabc307dab33e
Dana <danasnews1@gmail.com>
2020-12-10 10:18:03 -0600

    New and updated code

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,12 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "0bd9191d6a1b3af5f8156b1c90ba4dce"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBMRC-8V3uP8l3Kvb_cJx_4uzIslUitP6E"
+### Part II - VacationPy
+
+#Now let's use your skills in working with weather data to plan future vacations. Use jupyter-gmaps and the Google Places API for this part of the assignment.
+
+#* **Note:** Remember that any API usage beyond the $200 credit will be charged to your personal account. You can set quotas and limits to your daily requests to be sure you can't be charged. Check out [Google Maps Platform Billing](https://developers.google.com/maps/billing/gmp-billing#monitor-and-restrict-consumption) and [Manage your cost of use](https://developers.google.com/maps/documentation/javascript/usage-and-billing#set-caps) for more information.
+
+#https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=YOUR_API_KEY
